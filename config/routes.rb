087bd9fc64d1278623eebec31b0556b3b04ee667
Sandeep <sandeep@ubuntu.ubuntu-domain>
2013-06-12 12:25:01 +0530

    Added sitemap

--- config/routes.rb
+++ config/routes.rb
@@ -22,6 +22,7 @@ Golpo::Application.routes.draw do
   match '/demo', to: 'static_pages#demo'
   match '/about', to: 'static_pages#about'
   match '/contact', to: 'static_pages#contact'
+  match '/sitemap', to: 'static_pages#sitemap'
   match '/signup', to: 'users#new'
   match '/signin', to: 'sessions#new'
   match '/signout', to: 'sessions#destroy', via: :delete
